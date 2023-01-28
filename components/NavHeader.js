39a7dfd0a4b889d0697273dad8422465878d1c75
AguzZDev <example@example.com>
2023-01-28 12:28:35 -0300

    fix icons navbar

--- components/NavHeader.js
+++ components/NavHeader.js
@@ -6,8 +6,6 @@ import { SearchIcon } from "@heroicons/react/solid"
 
 import { IconSm } from "./Icon"
 import { numberAnimeRandom } from "utils/formats"
-import { ListIcon } from "public/svg/ListIcon"
-import { DiceIcon } from "public/svg/DiceIcon"
 
 export default function NavHeader() {
   const router = useRouter()
@@ -56,11 +54,29 @@ export default function NavHeader() {
         </div>
 
         <div className="flex flex-row items-center space-x-2 lg:space-x-5">
-          <NavItemIcon path="/" title="Mi lista" icon={<ListIcon />} />
+          <NavItemIcon
+            path="/"
+            title="Mi lista"
+            icon={
+              <svg
+                viewBox="0 0 48 48"
+                class="fill-current group-hover:text-orange text-gray-400"
+              >
+                <path d="M33.3,8H14.7C13.2,8,12,9.5,12,11v28.6c0,0.5,0.3,1,0.7,1.2c0.2,0.1,0.4,0.1,0.6,0.1c0.3,0,0.5-0.1,0.8-0.2l9.9-7.1l9.9,7.1c0.2,0.2,0.5,0.2,0.8,0.2c0.2,0,0.4,0,0.6-0.1c0.4-0.2,0.7-0.7,0.7-1.2V11C36,9.5,34.8,8,33.3,8z" />
+              </svg>
+            }
+          />
           <NavItemIcon
             path={`/${numberAnimeRandom()}`}
             title="Aleatorio"
-            icon={<DiceIcon />}
+            icon={
+              <svg
+                viewBox="0 0 58 46"
+                class="fill-current group-hover:text-orange text-gray-400"
+              >
+                <path d="M40,12.3C40,9.9,38.1,8,35.7,8H12.3C9.9,8,8,9.9,8,12.3v23.5c0,2.4,1.9,4.3,4.3,4.3h23.5c2.4,0,4.3-1.9,4.3-4.3V12.3zM16.3,19.7c-2.1,0-3.8-1.7-3.8-3.8s1.7-3.8,3.8-3.8c2.1,0,3.8,1.7,3.8,3.8S18.4,19.7,16.3,19.7z M32.2,35.6c-2.1,0-3.8-1.7-3.8-3.8s1.7-3.8,3.8-3.8c2.1,0,3.8,1.7,3.8,3.8S34.3,35.6,32.2,35.6z" />
+              </svg>
+            }
           />
 
           <form onSubmit={handleSubmit}>
@@ -90,7 +106,14 @@ export default function NavHeader() {
               <NavItemIcon
                 path={`/${numberAnimeRandom()}`}
                 title="Aleatorio"
-                icon={<DiceIcon />}
+                icon={
+                  <svg
+                    viewBox="0 0 58 46"
+                    class="fill-current group-hover:text-orange text-gray-400"
+                  >
+                    <path d="M40,12.3C40,9.9,38.1,8,35.7,8H12.3C9.9,8,8,9.9,8,12.3v23.5c0,2.4,1.9,4.3,4.3,4.3h23.5c2.4,0,4.3-1.9,4.3-4.3V12.3zM16.3,19.7c-2.1,0-3.8-1.7-3.8-3.8s1.7-3.8,3.8-3.8c2.1,0,3.8,1.7,3.8,3.8S18.4,19.7,16.3,19.7z M32.2,35.6c-2.1,0-3.8-1.7-3.8-3.8s1.7-3.8,3.8-3.8c2.1,0,3.8,1.7,3.8,3.8S34.3,35.6,32.2,35.6z" />
+                  </svg>
+                }
               />
             </div>
             <form onSubmit={handleSubmit}>
